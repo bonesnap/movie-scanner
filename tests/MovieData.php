7f593b42bd2e988ca8a2bcdd9dc3b74c8a5910e3
Brent Tilley <brent.tilley@gmail.com>
2026-05-03 21:10:44 -0400

    Added more test data and more tests.

--- tests/MovieData.php
+++ tests/MovieData.php
@@ -7,13 +7,17 @@ trait MovieData
 	public static function movieDataProvider() : array
 	{
 		$movies = [
-			'Apocalypse Now Redux (1979)'        => 'apocalypse',
-			'Last Action Hero (1993)'            => 'last',
-			'Mad Max: Fury Road (2015)'          => 'madmax',
-			'Oblivion (2013)'                    => 'oblivion',
-			'The Naked Gun (2025)'               => 'naked',
-			'The Smashing Machine (2025)'        => 'smashing',
-			'The Super Mario Bros. Movie (2023)' => 'mario',
+			'2 Guns (2013)'                                 => '2guns',
+			'Apocalypse Now Redux (1979)'                   => 'apocalypse',
+			'Last Action Hero (1993)'                       => 'last',
+			'Mad Max: Fury Road (2015)'                     => 'madmax',
+			'National Lampoon\'s Christmas Vacation (1989)' => 'vacation',
+			'Nosferatu (2024)'                              => 'nosferatu',
+			'Oblivion (2013)'                               => 'oblivion',
+			'The Big Lebowski (1998)'                       => 'lebowski',
+			'The Naked Gun (2025)'                          => 'naked',
+			'The Smashing Machine (2025)'                   => 'smashing',
+			'The Super Mario Bros. Movie (2023)'            => 'mario',
 		];
 
 		return array_map(function ($slug) {
