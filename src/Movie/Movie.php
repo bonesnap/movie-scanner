30761ba7a2f9c46448653e8dfd9fe76e64811e5d
Brent Tilley <brent.tilley@gmail.com>
2026-05-03 23:24:15 -0400

    Updated Movie.php class to collect and sort audio tracks.

--- src/Movie/Movie.php
+++ src/Movie/Movie.php
@@ -1,6 +1,7 @@
 <?php
 namespace Scanner\Movie;
 
+use Scanner\Data\Audio;
 use Scanner\Data\General;
 use Scanner\Data\Video;
 use Scanner\MediaInfo\MediaInfo;
@@ -10,6 +11,8 @@ class Movie
 	public readonly bool    $remux;
 	public readonly General $general;
 	public readonly Video   $video;
+	public readonly Audio   $primaryAudio;
+	public readonly Audio   $secondaryAudio;
 
 	public function __construct(private MediaInfo $mediaInfo) {}
 
@@ -22,10 +25,11 @@ class Movie
 
 	private function parseData(array $mediaInfo) : void
 	{
-		$tracks = $mediaInfo['media']['track'] ?? [];
+		$tracks      = $mediaInfo['media']['track'] ?? [];
+		$audioTracks = [];
 
 		if (empty($tracks)) {
-			return;
+			throw new \Exception('No media data!');
 		}
 
 		foreach ($tracks as $track) {
@@ -36,6 +40,36 @@ class Movie
 			if ($track['@type'] === 'Video' && $track['ID'] === '1') {
 				$this->video = new Video($track);
 			}
+
+			if (
+				$track['@type'] === 'Audio'
+				&& $track['Language'] === 'en'
+				&& !str_contains(strtolower($track['Title'] ?? ''), 'commentary')
+				&& !str_contains(strtolower($track['Title'] ?? ''), 'sound only')
+			) {
+				$audioTracks[] = new Audio($track);
+			}
 		}
+
+		$audioTracks = $this->sortAudioTracks($audioTracks);
+
+		$this->primaryAudio = $audioTracks[0];
+
+		if (isset($audioTracks[1])) {
+			$this->secondaryAudio = $audioTracks[1];
+		}
+	}
+
+	private function sortAudioTracks(array $audioTracks) : array
+	{
+		usort($audioTracks, function ($a, $b) {
+			/** @var Audio $a */
+			/** @var Audio $b */
+
+			// TODO: Should probably use bit rate and/or compression as the tie-breaker instead?
+			return Audio::AUDIO_NAMES[$b->name] <=> Audio::AUDIO_NAMES[$a->name];
+		});
+
+		return $audioTracks;
 	}
 }
