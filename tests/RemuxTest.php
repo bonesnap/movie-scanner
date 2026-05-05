f27ca44ea3e69c46f3c86ee905c0b0fdcaa54973
Brent Tilley <brent.tilley@gmail.com>
2026-05-04 23:35:05 -0400

    Refactored and corrected the RemuxTest.php suite.

--- tests/RemuxTest.php
+++ tests/RemuxTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Scanner\Data\DataException;
 use Scanner\MediaInfo\MediaInfo;
 use Scanner\Movie\Movie;
 
@@ -8,51 +9,51 @@ class RemuxTest extends TestCase
 {
 	public function testOblivionRemux() : void
 	{
-		$this->expectException(Exception::class);
-
-		$movie = new Movie(new MediaInfo());
-		$movie->analyze(new SplFileInfo('filenames/Oblivion (2013) - Remux.mkv'));
-
-		$this->assertTrue($movie->remux);
+		try {
+			$movie = new Movie(new MediaInfo());
+			$movie->analyze(new SplFileInfo('filenames/Oblivion (2013) - Remux.mkv'));
+		} catch (DataException) {
+			$this->assertTrue($movie->remux);
+		}
 	}
 
 	public function testLastActionHeroRemux() : void
 	{
-		$this->expectException(Exception::class);
-
-		$movie = new Movie(new MediaInfo());
-		$movie->analyze(new SplFileInfo('filenames/Last Action Hero (1993) - Remux.mkv'));
-
-		$this->assertTrue($movie->remux);
+		try {
+			$movie = new Movie(new MediaInfo());
+			$movie->analyze(new SplFileInfo('filenames/Last Action Hero (1993) - Remux.mkv'));
+		} catch (DataException) {
+			$this->assertTrue($movie->remux);
+		}
 	}
 
 	public function testApocalypseNowRemux() : void
 	{
-		$this->expectException(Exception::class);
-
-		$movie = new Movie(new MediaInfo());
-		$movie->analyze(new SplFileInfo('filenames/Apocalypse Now (1979) {edition-Redux} - Remux.mkv'));
-
-		$this->assertTrue($movie->remux);
+		try {
+			$movie = new Movie(new MediaInfo());
+			$movie->analyze(new SplFileInfo('filenames/Apocalypse Now (1979) {edition-Redux} - Remux.mkv'));
+		} catch (DataException) {
+			$this->assertTrue($movie->remux);
+		}
 	}
 
 	public function testSuperMarioBrosMovieRemux() : void
 	{
-		$this->expectException(Exception::class);
-
-		$movie = new Movie(new MediaInfo());
-		$movie->analyze(new SplFileInfo('filenames/The Super Mario Bros. Movie (2023) - Remux.mkv'));
-
-		$this->assertTrue($movie->remux);
+		try {
+			$movie = new Movie(new MediaInfo());
+			$movie->analyze(new SplFileInfo('filenames/The Super Mario Bros. Movie (2023) - Remux.mkv'));
+		} catch (DataException) {
+			$this->assertTrue($movie->remux);
+		}
 	}
 
 	public function testMadMaxFuryRoadRemux() : void
 	{
-		$this->expectException(Exception::class);
-
-		$movie = new Movie(new MediaInfo());
-		$movie->analyze(new SplFileInfo('filenames/Mad Max: Fury Road (2015).mkv'));
-
-		$this->assertFalse($movie->remux);
+		try {
+			$movie = new Movie(new MediaInfo());
+			$movie->analyze(new SplFileInfo('filenames/Mad Max: Fury Road (2015).mkv'));
+		} catch (DataException) {
+			$this->assertFalse($movie->remux);
+		}
 	}
 }
